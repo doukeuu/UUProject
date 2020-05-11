6c19237c0b514e1f8dc5fe228c349f5a0cf9a54f
DouKe <doukeuu@126.com>
2020-05-11 17:38:47 +0800

    ...

---
+++ OCProject/Facade/UICategory/UIView+UU.h
@@ -0,0 +1,30 @@
+//
+//  UIView+UU.h
+//  OCProject
+//
+//  Created by Pan on 2020/5/11.
+//  Copyright © 2020 xyz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIView (UU)
+
+// 简化view的frame相关参数获取代码
+@property (nonatomic, assign) CGFloat top;
+@property (nonatomic, assign) CGFloat left;
+@property (nonatomic, assign) CGFloat bottom;
+@property (nonatomic, assign) CGFloat right;
+
+@property (nonatomic, assign) CGFloat centerX;
+@property (nonatomic, assign) CGFloat centerY;
+
+@property (nonatomic, assign) CGFloat width;
+@property (nonatomic, assign) CGFloat height;
+@property (nonatomic, assign) CGSize size;
+@property (nonatomic, assign) CGPoint origin;
+@end
+
+NS_ASSUME_NONNULL_END
